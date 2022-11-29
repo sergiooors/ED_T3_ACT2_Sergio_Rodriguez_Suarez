72ee7b5f643f166fa9e9195dee6df0b6dfb36983
sergio <rodsuase@gregoriofer.local>
2022-11-29 09:05:03 +0100

    Primercommit

---
+++ Proyectos/Ejemplo1.java
@@ -0,0 +1,25 @@
+
+package Ejemplos;
+
+//este es mi primer ejercicio java
+//en el vamos a ver elementos basicos
+
+//import java.lang.System;
+
+public class Ejemplo1 {
+ /* la clase (class) es el elemento basico contenedor de un codigo fuente
+    que no se me olvide meter todo el codigo entre sus llaves
+    {y}
+    */   
+    
+    public static void main(String [] args){
+          System.out.println("hola como estamos"); 
+  
+int num=3;
+double exp= Math.pow(num, 3);
+        System.out.println( exp);    
+    
+    }  
+    
+    
+}
